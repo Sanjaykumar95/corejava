018a85012e00911946fe7283384a8bfd17c87a43
Sanjaykumar95 <ns49oq@gmail.com>
2017-04-03 00:26:58 +0530

    Add files via upload

--- fibonacci.java
+++ fibonacci.java
@@ -8,13 +8,18 @@ public static void main(String args[])
 	int f1=0 ,f2=1,f3=f1+f2,i,n;
 	Scanner in=new Scanner(System.in);
 	System.out.println("enter the limit");
-	n=in.nextInt();
-	while(n>f3)
-	{
-		f3=f1+f2;
-	f1=f2;
-	f2=f3;
-	System.out.print(f3+",");
+	try {
+		n=in.nextInt();
+		while(n>f3)
+		{
+			f3=f1+f2;
+		f1=f2;
+		f2=f3;
+		System.out.print(f3+",");
+		}
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
 }
 }
