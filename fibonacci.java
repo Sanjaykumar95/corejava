2147022f7fea430b704604bba8d45ae682c3ecaf
Sanjaykumar95 <ns49oq@gmail.com>
2017-04-03 00:24:52 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,20 @@
+package daysix;
+
+import java.util.Scanner;
+
+public class fibonacci {
+public static void main(String args[])
+{
+	int f1=0 ,f2=1,f3=f1+f2,i,n;
+	Scanner in=new Scanner(System.in);
+	System.out.println("enter the limit");
+	n=in.nextInt();
+	while(n>f3)
+	{
+		f3=f1+f2;
+	f1=f2;
+	f2=f3;
+	System.out.print(f3+",");
+	}
+}
+}
